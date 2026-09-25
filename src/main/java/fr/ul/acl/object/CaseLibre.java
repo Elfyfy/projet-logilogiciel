16a4c3bf62011186ff1c43eadf7e1817f6edb8ef
Maxence Guerguil <maxence.guerguil@gmail.com>
2026-09-25 16:39:59 +0200

    classe CaseLibre

--- src/main/java/fr/ul/acl/object/CaseLibre.java
+++ src/main/java/fr/ul/acl/object/CaseLibre.java
@@ -1,9 +1,18 @@
 package fr.ul.acl.object;
 
-public class CaseLibre {
+public class CaseLibre extends Case {
     private int nMines;
 
-    public CaseLibre(int x, int y) {
-        
+    public CaseLibre(int x, int y,String affichage) {
+        super(true,x,y,affichage);
+        nMines = 0;
+    }
+
+    // Getter et Setter :
+    public int getNMines() {
+        return nMines;
+    }
+    public void setNMines(int n) {
+        nMines = n;
     }
 }
